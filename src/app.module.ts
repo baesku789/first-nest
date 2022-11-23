2d8319e838911fb6266922860a22b10225825254
yechan <baesku789@gmail.com>
2022-11-23 13:56:46 +0900

    feat: logger middleware, exception filter 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
 
 @Module({
-  controllers: [CatsController],
-  providers: [CatsService],
+  imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(CatsController);
+  }
+}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cats.dto';
 import { Cat } from './interfaces/cat.interface';
+import { HttpExceptionFilter } from '../http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @UseFilters(HttpExceptionFilter)
   create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
