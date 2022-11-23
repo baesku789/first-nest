ffea0425890e34c3c29a183401cb379186d1fd76
yechan <baesku789@gmail.com>
2022-11-23 15:56:57 +0900

    refactor: - HttpExceptionFilter global 적용 basic guard 추가

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -6,18 +6,20 @@ import {
   ParseIntPipe,
   Post,
   UseFilters,
+  UseGuards,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cats.dto';
 import { Cat } from './interfaces/cat.interface';
 import { HttpExceptionFilter } from '../http-exception.filter';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('cats')
+@UseGuards(RolesGuard)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  @UseFilters(HttpExceptionFilter)
   create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
